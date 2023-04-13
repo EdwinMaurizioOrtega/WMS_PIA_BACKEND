2729565c01b80ea57979353ee4860e7dfbb3576a
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2023-04-13 12:28:21 -0500

    Creacion de las 4 APIs

--- app/controllers/WebServicesRemote.js
+++ app/controllers/WebServicesRemote.js
@@ -109,6 +109,7 @@ export const getRangoFechaCreacionPedidoProveedor = async (req, res) => {
     const params = [];
     const sql = "SELECT T0.PEDIDO_PROV,\n" +
         "       T0.FEC_INGRESO,\n" +
+        "       T0.FEC_ALTA,\n" +
         "       T0.USUARIO,\n" +
         "       T0.ESTATUS,\n" +
         "       T3.DESCRIPCION AS CLIENTE,\n" +
@@ -123,9 +124,7 @@ export const getRangoFechaCreacionPedidoProveedor = async (req, res) => {
         "       T0.FACTURA_FAB,\n" +
         "       T0.VAL1,\n" +
         "       T0.VAL2,\n" +
-        "       T0.PESO,\n" +
-        "       T0.FEC_ALTA,\n" +
-        "       T0.FEC_INGRESO\n" +
+        "       T0.PESO\n" +
         "FROM dbo.TD_CR_PEDIDO_PROV T0\n" +
         "         INNER JOIN dbo.TC_SOCIO_NEGOCIO T1 on T1.SOCIO = T0.SOCIO\n" +
         "         INNER JOIN dbo.TC_CR_PEDIDO_PROV_TIPO T2 ON T2.PEDIDO_PROV_TIPO = T0.PEDIDO_PROV_TIPO\n" +
@@ -151,14 +150,88 @@ export const getRangoFechaLlegadaPedidoProveedorBodega = async (req, res) => {
 
     const params = [];
     const sql = "SELECT T0.PEDIDO_PROV,\n" +
+        "       T0.FEC_INGRESO,\n" +
+        "       T0.FEC_ALTA,\n" +
+        "       T0.USUARIO,\n" +
+        "       T0.ESTATUS,\n" +
+        "       T3.DESCRIPCION AS CLIENTE,\n" +
+        "       T1.DESCRIPCION AS PROVEEDOR,\n" +
+        "       T2.DESCRIPCION,\n" +
+        "       T0.DATO1,\n" +
+        "       T0.DATO2,\n" +
+        "       T0.DATO3,\n" +
+        "       T0.DATO4,\n" +
+        "       T0.DATO5,\n" +
+        "       T0.FACTURA,\n" +
+        "       T0.FACTURA_FAB,\n" +
+        "       T0.VAL1,\n" +
+        "       T0.VAL2,\n" +
+        "       T0.PESO\n" +
+        "FROM dbo.TD_CR_PEDIDO_PROV T0\n" +
+        "         INNER JOIN dbo.TC_SOCIO_NEGOCIO T1 on T1.SOCIO = T0.SOCIO\n" +
+        "         INNER JOIN dbo.TC_CR_PEDIDO_PROV_TIPO T2 ON T2.PEDIDO_PROV_TIPO = T0.PEDIDO_PROV_TIPO\n" +
+        "         INNER JOIN dbo.TC_CR_PEDIDO_PROV_TIPO T3 ON T3.PEDIDO_PROV_TIPO = T0.PEDIDO_PROV_TIPO\n" +
+        "WHERE T0.FEC_INGRESO BETWEEN CAST('"+fec_inicio+" 00:00:00' AS datetime )  AND  CAST('"+fec_fin+" 23:59:59' AS datetime)";
+    // For each param do: db.buildParams(params, "name", TYPES.type, variable)
+    //myObject.buildParams(params, "number", TYPES.Int, number);
+    myObject.query(params, sql, result => {
+        console.log(result);
+        res.send({
+            "data": result
+        })
+    });
+
+};
+
+
+export const getDespachoPedidoProveedor = async (req, res) => {
+
+    // FECHA DE LLEGADA (FEC_INGRESO) A BODEGA
+
+    const n_pedido = req.query.n_pedido; //11681
+    const procedencia = req.query.procedencia; //7001
+
+    const params = [];
+    const sql = "SELECT T0.NUM_PEDIDO,\n" +
+        "       T0.PROCEDENCIA,\n" +
+        "       T0.FECHA,\n" +
+        "       T0.CONTACTO,\n" +
+        "       T0.TEL_CONTACTO,\n" +
+        "       T0.CANTIDAD,\n" +
+        "       T0.TOTAL,\n" +
+        "       T1.DESCRIPCION\n" +
+        "FROM dbo.TD_CR_PEDIDO T0\n" +
+        "         INNER JOIN dbo.TC_CR_CLIENTE T1 ON T1.CTE = T0.CTE and T1.CTE_PROCEDE = T0.CTE_PROCEDE\n" +
+        "WHERE T0.NUM_PEDIDO = "+n_pedido+"\n" +
+        "  AND T0.PROCEDENCIA = "+procedencia+"";
+    // For each param do: db.buildParams(params, "name", TYPES.type, variable)
+    //myObject.buildParams(params, "number", TYPES.Int, number);
+    myObject.query(params, sql, result => {
+        console.log(result);
+        res.send({
+            "data": result
+        })
+    });
+
+};
+
+
+
+export const getDespachoDetallePedidoProveedor = async (req, res) => {
+
+    const n_pedido = req.query.n_pedido; //439
+    const procedencia = req.query.procedencia; //7001
+
+    const params = [];
+    const sql = "SELECT T0.NUM_PEDIDO,\n" +
         "       T0.PROCEDENCIA,\n" +
         "       T0.ARTICULO,\n" +
-        "       T0.SERIE,\n" +
-        "       T1.DESCRIPCION,\n" +
-        "       T1.PESO\n" +
-        "FROM TR_CR_PEDIDO_PROV_SERIE T0\n" +
-        "         INNER JOIN TC_CR_ARTICULO T1 ON T1.ARTICULO = T0.ARTICULO AND T1.ART_PROCEDE = T0.PROCEDENCIA\n" +
-        "    AND T0.PEDIDO_PROV = "+n_pedido+" and T0.PROCEDENCIA = "+procedencia+"";
+        "       T0.CANTIDAD,\n" +
+        "       T1.DESCRIPCION\n" +
+        "FROM dbo.TD_CR_PEDIDO_DET T0\n" +
+        "         INNER JOIN dbo.TC_CR_ARTICULO T1 ON T1.ARTICULO = T0.ARTICULO AND T1.ART_PROCEDE = T0.ART_PROCEDE\n" +
+        "WHERE NUM_PEDIDO = "+n_pedido+"\n" +
+        "  AND PROCEDENCIA = "+procedencia+"";
     // For each param do: db.buildParams(params, "name", TYPES.type, variable)
     //myObject.buildParams(params, "number", TYPES.Int, number);
     myObject.query(params, sql, result => {
--- app/routes/crm.routes.js
+++ app/routes/crm.routes.js
@@ -3,23 +3,21 @@ import express from 'express';
 //MongoDB - WMS
 import { createRegistroImagen, getListImageByProveedorAndProcedencia } from '../controllers/ValidacionImei.js';
 
-
 //MicrosoftSQLServer WMS
 import {
     getPedidoProveedor,
     getDetallePedidoProveedor,
     getRangoFechaCreacionPedidoProveedor,
     getRangoFechaLlegadaPedidoProveedorBodega,
-    getCantidadDetallePedidoProveedor
+    getCantidadDetallePedidoProveedor,
+    getDespachoPedidoProveedor,
+    getDespachoDetallePedidoProveedor
+
 } from '../controllers/WebServicesRemote.js';
 import { IniciarSesion } from '../controllers/user.js'
 
-
 //HT-BUSINESS HANA DB
 import { getSociosNegocio, getCondicionPago } from '../controllers/SociosDeNegocios.js';
-
-
-
 const router = express.Router();
 
 //Cargar imagenes en MongoDB
@@ -30,8 +28,10 @@ router.get('/lista_imagenes', getListImageByProveedorAndProcedencia);
 router.get('/reporte_pedido_proveedor', getPedidoProveedor);
 router.get('/reporte_detalle_pedido_proveedor', getDetallePedidoProveedor);
 router.get('/reporte_cantidad_detalle_pedido_proveedor', getCantidadDetallePedidoProveedor);
-router.get('/rango_fecha_creacion_pedido_proveedor', getRangoFechaCreacionPedidoProveedor)
-router.get('/rango_fecha_llegada_pedido_proveedor_bodega', getRangoFechaLlegadaPedidoProveedorBodega)
+router.get('/rango_fecha_creacion_pedido_proveedor', getRangoFechaCreacionPedidoProveedor);
+router.get('/rango_fecha_llegada_pedido_proveedor_bodega', getRangoFechaLlegadaPedidoProveedorBodega);
+router.get('/reporte_despacho_pedido_proveedor', getDespachoPedidoProveedor);
+router.get('/reporte_despacho_detalle_pedido_proveedor', getDespachoDetallePedidoProveedor);
 
 
 
